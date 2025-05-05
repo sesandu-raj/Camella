31afeec39be18a04dc39ca3a3505f707b8b8c605
HMSRA Jayathissa <sesanduj@gmail.com>
2025-05-05 16:28:51 +0530

    Change in JavScript File 3

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 console.log("Hello, World!");
 console.log("This is a test script.");
-console.log("The script is running successfully.");
-console.log("Adding more logs to see the output.");
-console.log("Logging is a useful tool for debugging.");
+
 // This is a test script to demonstrate logging in JavaScript.
