f70c145412381a03deec5b0ca502211eca78f24b
HMSRA Jayathissa <sesanduj@gmail.com>
2025-05-05 16:30:50 +0530

    Change in JavScript File 4

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Hello, World!");
 console.log("This is a test script.");
-
+console.log("Logging in JavaScript is easy!");
+console.log("You can log any type of data.");
+console.log("For example, numbers: ", 42);
 // This is a test script to demonstrate logging in JavaScript.
